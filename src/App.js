c3a55edcbf1c19e01989e2e88a33194df09c9909
amisadit <34134006+amisadit@users.noreply.github.com>
2021-01-27 14:53:46 +0600

    Comments and Part-1 of the video is done!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React, { Component} from 'react';
+import Button from "./components/Button.js";
 import './App.css';
 
-class App extends Comment {
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+
+      current: '',
+      previous: []
+    }
+  }
+
+  reset = () => {
+
+    this.setState({result: '0'});
+  }
+
+  addToCurrent = (symbols) => {
+
+    this.setState({current: this.state.current + symbols});
+
+
+  }
 
   render() {
 
+    const buttons = [
+      //This is an object
+      {symbols: 'C', cols: 1, action: this.reset},
+      {symbols: '/', cols: 3, action: this.addToCurrent},
+      {symbols: '7', cols: 1, action: this.addToCurrent},
+      {symbols: '8', cols: 1, action: this.addToCurrent},
+      {symbols: '9', cols: 1, action: this.addToCurrent},
+      {symbols: 'X', cols: 1, action: this.addToCurrent},
+      {symbols: '4', cols: 1, action: this.addToCurrent},
+      {symbols: '5', cols: 1, action: this.addToCurrent},
+      {symbols: '6', cols: 1, action: this.addToCurrent},
+      {symbols: '-', cols: 1, action: this.addToCurrent},
+      {symbols: '1', cols: 1, action: this.addToCurrent},
+      {symbols: '2', cols: 1, action: this.addToCurrent},
+      {symbols: '3', cols: 1, action: this.addToCurrent},
+      {symbols: '+', cols: 1, action: this.addToCurrent},
+      {symbols: '0', cols: 2, action: this.addToCurrent},
+      {symbols: '.', cols: 1, action: this.addToCurrent},
+      {symbols: '=', cols: 1, action: this.addToCurrent},
+
+    ];
+
     return (
 
-      <h1>Hello World!</h1>
+      <div className="App">
+        <input className="result" type="text" value={this.state.current}></input>
+
+      
+        {buttons.map((btn, i)=> { //Buttons will appear based on the array along with symbols
+
+          return <Button symbols={btn.symbols} cols={btn.cols} action={(symbols) => btn.action}/>
+
+        })}
+      </div>
 
     );
   }
