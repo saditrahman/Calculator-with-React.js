4c1d1234af12b063f7e0a2c77056b3847a5324ca
amisadit <34134006+amisadit@users.noreply.github.com>
2021-01-28 13:48:43 +0600

    Changed the entire codebase as it wasn't working!

--- src/App.js
+++ src/App.js
@@ -1,71 +1,104 @@
-import React, { Component} from 'react';
-import Button from "./components/Button.js";
-import './App.css';
-
+import React, { Component } from "react";
+import Button from "./components/Button";
+import "./css/style.css";
 class App extends Component {
-
   constructor(props) {
-
     super(props);
 
     this.state = {
-
-      current: '',
-      previous: []
-    }
+      current: "0",
+      previous: [],
+      nextIsReset: false,
+    };
   }
 
   reset = () => {
+    this.setState({ current: "0", previous: [], nextIsReset: false });
+  };
 
-    this.setState({result: '0'});
-  }
+  addToCurrent = (symbol) => {
+    console.log(symbol);
+    if (["/", "*", "-", "+"].indexOf(symbol) > -1) {
+      let { previous } = this.state;
+      previous.push(this.state.current + " " + symbol);
+      this.setState({ previous, nextIsReset: true });
+    } else {
+      if (
+        (this.state.current === "0" && symbol !== ".") ||
+        this.state.nextIsReset
+      ) {
+        this.setState({ current: symbol, nextIsReset: false });
+      } else {
+        this.setState({ current: this.state.current + symbol });
+      }
+    }
+  };
 
-  addToCurrent = (symbols) => {
+  calculate = (symbol) => {
+    let { current, previous } = this.state;
+    if (previous.length > 0) {
+      current = eval(String(previous[previous.length - 1] + current));
 
-    this.setState({current: this.state.current + symbols});
+      this.setState({ current, previous: [], nextIsReset: true });
+    }
+  };
 
-
-  }
+  backSpace = () => {
+    let { current } = this.state;
+    if (current.length >= 2) {
+      this.setState({ current: current.slice(0, -1) });
+    } else {
+      this.setState({ current: "0" });
+    }
+  };
 
   render() {
-
     const buttons = [
-      //This is an object
-      {symbols: 'C', cols: 1, action: this.reset},
-      {symbols: '/', cols: 3, action: this.addToCurrent},
-      {symbols: '7', cols: 1, action: this.addToCurrent},
-      {symbols: '8', cols: 1, action: this.addToCurrent},
-      {symbols: '9', cols: 1, action: this.addToCurrent},
-      {symbols: 'X', cols: 1, action: this.addToCurrent},
-      {symbols: '4', cols: 1, action: this.addToCurrent},
-      {symbols: '5', cols: 1, action: this.addToCurrent},
-      {symbols: '6', cols: 1, action: this.addToCurrent},
-      {symbols: '-', cols: 1, action: this.addToCurrent},
-      {symbols: '1', cols: 1, action: this.addToCurrent},
-      {symbols: '2', cols: 1, action: this.addToCurrent},
-      {symbols: '3', cols: 1, action: this.addToCurrent},
-      {symbols: '+', cols: 1, action: this.addToCurrent},
-      {symbols: '0', cols: 2, action: this.addToCurrent},
-      {symbols: '.', cols: 1, action: this.addToCurrent},
-      {symbols: '=', cols: 1, action: this.addToCurrent},
-
+      { symbol: "C", cols: 2, action: this.reset },
+      { symbol: "DEL", cols: 1, action: this.backSpace },
+      { symbol: "/", cols: 1, action: this.addToCurrent },
+      { symbol: "7", cols: 1, action: this.addToCurrent },
+      { symbol: "8", cols: 1, action: this.addToCurrent },
+      { symbol: "9", cols: 1, action: this.addToCurrent },
+      { symbol: "*", cols: 1, action: this.addToCurrent },
+      { symbol: "4", cols: 1, action: this.addToCurrent },
+      { symbol: "5", cols: 1, action: this.addToCurrent },
+      { symbol: "6", cols: 1, action: this.addToCurrent },
+      { symbol: "-", cols: 1, action: this.addToCurrent },
+      { symbol: "1", cols: 1, action: this.addToCurrent },
+      { symbol: "2", cols: 1, action: this.addToCurrent },
+      { symbol: "3", cols: 1, action: this.addToCurrent },
+      { symbol: "+", cols: 1, action: this.addToCurrent },
+      { symbol: "0", cols: 2, action: this.addToCurrent },
+      { symbol: ".", cols: 1, action: this.addToCurrent },
+      { symbol: "=", cols: 1, action: this.calculate },
     ];
 
     return (
-
       <div className="App">
-        <input className="result" type="text" value={this.state.current}></input>
+        <div>
+          {this.state.previous.length > 0 ? (
+            <div className="prev-display">
+              {this.state.previous[this.state.previous.length - 1]}
+            </div>
+          ) : null}
+          <div className="result"> {this.state.current}</div>
+        </div>
 
-      
-        {buttons.map((btn, i)=> { //Buttons will appear based on the array along with symbols
-
-          return <Button symbols={btn.symbols} cols={btn.cols} action={(symbols) => btn.action}/>
-
+        <br />
+        {buttons.map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              symbol={btn.symbol}
+              cols={btn.cols}
+              action={(symbol) => btn.action(symbol)}
+            ></Button>
+          );
         })}
       </div>
-
     );
-  }
+  } w
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,17 @@
-import React, { Component} from "react";
+import React, { Component } from 'react'
 
 class Button extends Component {
-
-    render() {
-
-        return(
-
-            <div>
-
-                <button class="calc-button">{this.props.symbols}</button>
-            </div>
-
-
-        );
-    }
+  render() {
+    return (
+      <div className={`column-${this.props.cols}`}>
+        <button
+          className="calc-button"
+          onClick={() => this.props.action(this.props.symbol)}
+        >
+          {this.props.symbol}
+        </button>
+      </div>
+    );
+  }
 }
-
-export default Button;
+export default Button
